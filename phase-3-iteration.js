6305f2b9bdd74fcc4b4176f88bfe2671e4b7831f
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-27 16:33:24 -0800

    finish phase 3

--- phase-3-iteration.js
+++ phase-3-iteration.js
@@ -1,1 +1,35 @@
 // Your code here
+Array.prototype.uniq= function(){
+    let result = [];
+    this.forEach((el)=>
+    {if( result.indexOf(el) === -1)
+        {result.push(el)}});
+    return result
+}
+
+Array.prototype.bubbleSort = function(){
+    let sorted = false;
+    while(!sorted){
+        sorted = true
+        for(i = 0; i < this.length - 1; i++){
+            if(this[i] > this[i + 1]){
+                sorted = false;
+                [this[i], this[i+1]] = [this[i+1], this[i]];
+            }
+        }
+    }
+    return this
+}
+String.prototype.substrings = function(){
+    let result = [];
+    for(i = 0; i < this.length - 1; i++){
+        let substring = this[i];
+        result.push(substring);
+        for(j = i + 1; j < this.length; j++){
+            substring += this[j];
+            result.push(substring);
+        }
+    }
+    result.push(this[this.length - 1]);
+    return result.uniq()
+}
