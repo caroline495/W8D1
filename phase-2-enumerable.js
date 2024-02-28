0b99a56c0042529f3b5cd87e3d7ca039bf9c0950
Caroline Zhang <cczhang1@yahoo.com>
2024-02-27 16:08:06 -0800

    Finish phase 2

--- phase-2-enumerable.js
+++ phase-2-enumerable.js
@@ -1,1 +1,34 @@
-// Your code here
+Array.prototype.myEach = function(callback) {
+    for (i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+}
+
+const doubler = number => number * 2;
+
+Array.prototype.myMap = function(callback) {
+    let result = [];
+    this.myEach((el)=>{
+        let newEl = callback(el);
+        result.push(newEl);
+    })
+    return result;
+}
+
+Array.prototype.myReduce = function(callback) {
+    let initialValue = this[0];
+    let sliceIndex = 1;
+    if (arguments.length > 1) {
+        initialValue = arguments[1];
+        sliceIndex = 0;
+    }
+
+    let result = initialValue;
+
+    this.slice(sliceIndex).myEach((ele)=> {
+        result = callback(result, ele);
+    })
+    return result;
+} 
+
+
